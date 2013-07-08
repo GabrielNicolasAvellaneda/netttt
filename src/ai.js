a5ac18f37ed0f63ec18bcb6c10c5a5c41e848bf9
Charles Lindsay <chaz@chazomatic.us>
2013-07-07 19:08:31 -0700

    Move negamax to prototype, add TODOs, clean up

--- src/ai.js
+++ src/ai.js
@@ -35,14 +35,12 @@ var Ai = (function (Ai) {
   }
 
   // We give a winning position a high score, then count the number of ways a
-  // player could win at the current position.  We use depth to nudge us to
-  // winning sooner over later.
-  function evaluate(board, depth, winner) {
-    depth = depth || 0;
+  // player could win at the current position.
+  function evaluate(board, winner) {
     if (typeof winner === 'undefined')
       winner = Ttt.winner(board);
     if (winner)
-      return sign(winner === Ttt.TIE ? 0 : winner) * (100 + depth);
+      return sign(winner === Ttt.TIE ? 0 : winner) * 100;
 
     return countNearWins(board) * 10;
   }
@@ -62,31 +60,39 @@ var Ai = (function (Ai) {
     return best;
   }
 
+  function Smart(maxDepth) {
+    this.maxDepth = maxDepth || 4;
+  }
+
   // Slightly modified negamax with alpha-beta pruning (see
   // <http://en.wikipedia.org/wiki/Negamax> or
   // <http://www.hamedahmadi.com/gametree/>).  Because we use this to instruct
-  // us as to which move to make, if top is true we return the move instead of
-  // the score of the best move.
-  function negamax(board, turn, depth, top, alpha, beta) {
+  // us as to which move to make, if we're at the top level we return the move
+  // instead of the score of the best move.
+  Smart.prototype.negamax = function (board, turn, depth, alpha, beta) {
+    // TODO: keep a map of boards -> their value for each search, so we don't
+    // constantly re-evaluate the same thing.
     var winner = Ttt.winner(board);
-    if (!depth || winner)
-      return sign(turn) * evaluate(board, depth, winner);
+    if (depth === this.maxDepth || winner)
+      return sign(turn) * evaluate(board, winner);
 
     var max = -Infinity;
     var potentials = [];
+    // TODO: use symmetry (at least for the first two moves) to avoid searching
+    // the full set of valid moves.
     // TODO: "branching", to try the likely-best move first.
     var moves = Ttt.validMoves(board);
     for (var move in moves) {
       move = moves[move];
 
-      var value = -negamax(
+      var value = -this.negamax(
         Ttt.move(board, move, turn),
         (turn === Ttt.X ? Ttt.O : Ttt.X),
-        depth - 1, false, -beta, -alpha
+        depth + 1, -beta, -alpha
       );
 
       if (value >= beta)
-        return (top ? move : value);
+        return (depth ? value : move);
       if (value > alpha)
         alpha = value;
       if (value > max) {
@@ -97,19 +103,16 @@ var Ai = (function (Ai) {
       }
     }
 
-    return (top ? resolveTies(board, potentials, turn) : max);
-  }
-
-  function Smart(depth) {
-    this.depth = depth || 4;
+    return (depth ? max : resolveTies(board, potentials, turn));
   }
 
   Smart.prototype.getMove = function (game) {
-    return negamax(game.board, game.turn, this.depth, true, -Infinity, Infinity);
+    return this.negamax(game.board, game.turn, 0, -Infinity, Infinity);
   };
 
   Ai.Random = Random;
   Ai.Smart = Smart;
+  Ai.evaluate = evaluate;
 
   return Ai;
 }(Ai || {}));
