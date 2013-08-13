bab0585143ae3c20628f8c90702a23718c6b9cc5
Charles Lindsay <chaz@chazomatic.us>
2013-08-13 00:16:39 -0700

    Add worker logic

---
+++ main.worker.js
@@ -0,0 +1,35 @@
+(function (global) {
+    "use strict";
+
+    global.onmessage = function (event) {
+        var result = process(event.data);
+        postMessage(result);
+    };
+
+    function process(data) {
+        var generation = importGeneration(data);
+        generation.run();
+        return exportResult(generation);
+    }
+
+    function importGeneration(data) {
+        var id = data.generation;
+        var exports = data.individuals;
+
+        return new NetTtt.Generation(id, exports.map(function (e) {
+            return NetTtt.Individual.import(e);
+        }));
+    }
+
+    function exportResult(generation) {
+        return {
+            generation: generation.id,
+            scores: generation.members.map(function (m) {
+                return {
+                    id: m.individual.id,
+                    score: m.score
+                };
+            })
+        };
+    }
+}(this));
