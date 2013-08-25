b16b3959afdb01e207ab9a885563eaa12ae00b13
Charles Lindsay <chaz@chazomatic.us>
2013-08-24 20:03:10 -0700

    Add printout of top net sizes

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ $(function () {
     var $leaders = best.map(function (b, i) {
         return $('#leader-' + i.toString());
     });
+    var $exportSizes = $('#export-sizes');
     var $topExport = $('#top-export');
 
     var graphCtx = $graph[0].getContext('2d');
@@ -193,6 +194,11 @@ $(function () {
 
         if (topChanged) {
             $topExport.text(JSON.stringify(best[0].individual.export()));
+            var sizes = best[0].individual.net.getSizes().slice(1, -1);
+            $exportSizes.text(
+                $exportSizes.data('template')
+                .replace('{sizes}', sizes.toString())
+            );
 
             resetDemos();
         }
