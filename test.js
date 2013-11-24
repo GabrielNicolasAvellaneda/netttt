ddd328bdd7cf496013409e02d258de927e4bbe54
Charles Lindsay <chaz@chazomatic.us>
2013-11-23 18:28:55 -0800

    GPL license

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
+/*
+   netttt - evolving neural networks to play tic tac toe
+            <https://github.com/chazomaticus/netttt>
+   Copyright 2013 Charles Lindsay
+
+   netttt is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+
+   netttt is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along with
+   netttt.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 "use strict";
 
 module("Ttt");
